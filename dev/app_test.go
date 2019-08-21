b87da2d332fd051013fef572579d628de64aa2b4
Alan Norton <me@alannorton.com>
2019-08-21 07:52:59 -0400

    wip

--- dev/app_test.go
+++ dev/app_test.go
@@ -1,49 +1,40 @@
-package dev_test
+package dev
 
 import (
+	"os/exec"
 	"testing"
 	"time"
-	"github.com/stretchr/testify/mock"
-	. "github.com/puma/puma-dev/dev"
+	// "github.com/stretchr/testify/mock"
+	// . "github.com/puma/puma-dev/dev"
 )
 
-type MockAppPool struct{
-  mock.Mock
-}
-
-type MockCommand struct{
-	mock.Mock
-}
-
-type MockEvents struct {
-	mock.Mock
-}
-
-func TestAppKill(t *testing.T) {
-	stdout, err := cmd.StdoutPipe()
+func TestAppKillAppPoolRemoval(t *testing.T) {
+	cmd := exec.Command("bash", "-c", "sleep 600")
+	err := cmd.Start()
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	mockPool := new(MockAppPool)
-	mockCmd := new(MockCommand)
-	mockEvents := new(MockEvents)
+	t.Logf("%+v\n", cmd.Process)
+	t.Log(cmd.Process.Pid)
 
-	mockPool.On("Events").Return(mockEvents, nil)
+	var events Events
+	var pool AppPool
+
+	pool.Dir = "/tmp/puma-dev"
+	pool.IdleTime = 15 * 60 * time.Second
+	pool.Events = &events
 
 	app := &App{
 		Name:      "app",
-		Command:   mockCmd,
-		Events:    mockPool.Events,
-		stdout:    stdout,
-		dir:       "/",
-		pool:      mockPool,
+		Command:   cmd,
+		Events:    pool.Events,
 		readyChan: make(chan struct{}),
-		lastUse:   time.Now().Add(time.Duration(-60) * time.Minute),
 	}
 
-	t.Logf(app.Name)
+	t.Logf("%+v\n", app)
+	t.Logf("%+v\n", app.Events)
 
 	app.Kill("just because")
 }
