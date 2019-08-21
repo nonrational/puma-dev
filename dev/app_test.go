e7ec488fa8c9817e42e5d850a0e750388e014363
Alan Norton <me@alannorton.com>
2019-08-20 21:58:54 -0400

    🐰🕳

--- dev/app_test.go
+++ dev/app_test.go
@@ -1,17 +1,49 @@
-package dev
+package dev_test
 
 import (
 	"testing"
-	"github.com/stretchr/testify/assert"
-	"github.com/vektra/neko"
+	"time"
+	"github.com/stretchr/testify/mock"
+	. "github.com/puma/puma-dev/dev"
 )
 
-func TestApp(t *testing.T) {
-  n := neko.Start(t)
+type MockAppPool struct{
+  mock.Mock
+}
 
-	n.It("verifies sanity", func() {
-		assert.Equal(t, 1, 1)
-	})
+type MockCommand struct{
+	mock.Mock
+}
 
-  n.Meow()
+type MockEvents struct {
+	mock.Mock
+}
+
+func TestAppKill(t *testing.T) {
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mockPool := new(MockAppPool)
+	mockCmd := new(MockCommand)
+	mockEvents := new(MockEvents)
+
+	mockPool.On("Events").Return(mockEvents, nil)
+
+	app := &App{
+		Name:      "app",
+		Command:   mockCmd,
+		Events:    mockPool.Events,
+		stdout:    stdout,
+		dir:       "/",
+		pool:      mockPool,
+		readyChan: make(chan struct{}),
+		lastUse:   time.Now().Add(time.Duration(-60) * time.Minute),
+	}
+
+	t.Logf(app.Name)
+
+	app.Kill("just because")
 }
