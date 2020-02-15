451772582dc773066bbfcfc4b3032c5c97a16c27
Alan Norton <me@alannorton.com>
2020-02-15 08:27:44 -0500

    make sure that we create working directory before using it

--- cmd/puma-dev/command_test.go
+++ cmd/puma-dev/command_test.go
@@ -44,6 +44,7 @@ func TestCommand_link_noArgs(t *testing.T) {
 
 func TestCommand_link_withNameOverride(t *testing.T) {
 	tmpCwd := "/tmp/puma-dev-example-command-link-noargs"
+	defer MakeDirectoryOrFail(t, tmpCwd)()
 
 	StubCommandLineArgs("link", "-n", "anothername", tmpCwd)
 
--- dev/devtest/testutils.go
+++ dev/devtest/testutils.go
@@ -124,7 +124,7 @@ func RemoveDirectoryOrFail(t *testing.T, path string) {
 // MakeDirectoryOrFail makes a directory or fails the test, returning the path
 // of the directory that was created.
 func MakeDirectoryOrFail(t *testing.T, path string) func() {
-	if err := os.Mkdir(path, 0755); err != nil {
+	if err := os.MkdirAll(path, 0755); err != nil {
 		assert.Fail(t, err.Error())
 	}
 
