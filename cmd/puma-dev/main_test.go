2cc02acb9225ecfe8ee513a9862c548130c312d5
Alan Norton <me@alannorton.com>
2020-03-05 12:00:33 -0500

    provide additional headers to allow Host and Puma-Dev-Host to be used independently.
    try a different header approach
    
    use Host header iff url hostname is an ip address
    
    use urls on travis
    
    use travis config to set hosts
    
    add failing test
    
    not sure if this is the direction we should go in, but it makes the tests pass
    
    cleanup, simplification of testcases for app name resolution
    
    PumaDevRequest wraps a http.Request and can determine appName from headers
    
    lost a lot of debugging time here. #imissrubylibs
    
    extract request to new file. it handles all the logic for app names now
    
    don't use addresses that don't exist in travis host overrides, remove app name override

--- cmd/puma-dev/main_test.go
+++ cmd/puma-dev/main_test.go
@@ -75,6 +75,18 @@ func launchPumaDevBackgroundServerWithDefaults(t *testing.T) func() {
 func getURLWithHost(t *testing.T, url string, host string) string {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Host = host
+	// req.Header.Add("Puma-Dev-Host", host)
+	// req.Host = req.URL.Host
+
+	// for hk, hv := range headers {
+	// 	req.Header.Add(hk, hv)
+
+	// 	// http.Client doesn't automatically respect Host header; have to manually set req.Host.
+	// 	// https://github.com/golang/go/issues/7682
+	// 	if hk == "Host" {
+	// 		req.Host = hv
+	// 	}
+	// }
 
 	resp, err := http.DefaultClient.Do(req)
 
---
+++ dev/request_test.go
@@ -0,0 +1,79 @@
+package dev
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testPumaDevRequest PumaDevRequest
+
+type PumaDevRequestCtx struct {
+	URL         string
+	Host        string
+	PumaDevHost string
+}
+
+func (ctx *PumaDevRequestCtx) PumaDevReq() PumaDevRequest {
+	var r io.Reader
+
+	req, _ := http.NewRequest("GET", ctx.URL, r)
+	req.Host = req.URL.Host
+
+	if ctx.Host != "" {
+		req.Header.Add("Host", ctx.Host)
+		req.Host = ctx.Host
+	}
+
+	if ctx.PumaDevHost != "" {
+		req.Header.Add("Puma-Dev-Host", ctx.PumaDevHost)
+	}
+
+	return PumaDevRequest{req}
+}
+
+func TestPumaDevRequest_AppName(t *testing.T) {
+
+	testCases := map[PumaDevRequestCtx]string{
+		PumaDevRequestCtx{URL: "http://qwerty.puma", PumaDevHost: "asdf.puma"}: "asdf",
+		PumaDevRequestCtx{URL: "http://qwerty.puma", PumaDevHost: "asdf"}:      "asdf",
+		PumaDevRequestCtx{URL: "http://qwerty.puma", Host: "asdf.puma"}:        "asdf",
+		PumaDevRequestCtx{URL: "http://qwerty.puma"}:                           "qwerty",
+
+		PumaDevRequestCtx{URL: "https://127.0.0.1:443", PumaDevHost: "asdf.puma"}: "asdf",
+		PumaDevRequestCtx{URL: "https://127.0.0.1:443", PumaDevHost: "asdf"}:      "asdf",
+		PumaDevRequestCtx{URL: "https://127.0.0.1:443", Host: "asdf.puma"}:        "asdf",
+		PumaDevRequestCtx{URL: "https://my.app.with.puma"}:                        "my.app.with",
+
+		PumaDevRequestCtx{URL: "http://127.0.0.1", Host: "proxy.io", PumaDevHost: "asdf.puma"}: "asdf",
+		PumaDevRequestCtx{URL: "http://127.0.0.1", Host: "proxy.io", PumaDevHost: "asdf"}:      "asdf",
+	}
+
+	for ctx, expectedAppName := range testCases {
+		t.Run(ctx.URL, func(t *testing.T) {
+			pdReq := ctx.PumaDevReq()
+			assert.Equal(t, expectedAppName, pdReq.AppName())
+		})
+	}
+}
+
+func TestPumaDevRequest_canonicalAppNameFromHost(t *testing.T) {
+	testCases := map[string]string{
+		"app":                        "app",
+		"app.test":                   "app",
+		"app.dev:8080":               "app",
+		"app.0.0.0.0.xip.io":         "app",
+		"app.255.255.255.255.nip.io": "app",
+		"app.loc.al":                 "app.loc",
+		"app.0.0.xip.io":             "",
+	}
+
+	for host, expectedAppName := range testCases {
+		t.Run(host, func(t *testing.T) {
+			appName := testPumaDevRequest.canonicalAppNameFromHost(host)
+			assert.Equal(t, appName, expectedAppName)
+		})
+	}
+}
