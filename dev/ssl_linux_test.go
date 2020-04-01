48f80f8a150c8fad5981dfa568bfbf2e18bbff6d
Alan Norton <me@alannorton.com>
2020-04-01 12:07:30 -0400

    forgot one

--- dev/ssl_linux_test.go
+++ dev/ssl_linux_test.go
@@ -8,11 +8,12 @@ import (
 )
 
 func TestTrustCert_Linux_noCertProvided(t *testing.T) {
-	stdOut := WithStdoutCaptured(func() {
-		// TrustCert on Linux is a no-op, so it "succeeds" even if the path isn't found
-		err := TrustCert("/does/not/exist")
-		assert.Nil(t, err)
-	})
+	resetAndRead := CaptureStdout()
+
+	err := TrustCert("/does/not/exist")
+	assert.Nil(t, err)
+
+	stdOut, _ := resetAndRead()
 
 	assert.Regexp(t, "^! Add /does/not/exist to your browser to trust CA\\n$", stdOut)
 }
